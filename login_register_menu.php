2c737e40700f0b56a83ae6196a63a0c3558b63fa
Mohd Alam <mohdalam392@gmail.com>
2016-04-08 11:29:46 +0530

    Menu Added To Primary Menu Change
    Menu Added To Primary Menu Change

--- login_register_menu.php
+++ login_register_menu.php
@@ -9,26 +9,29 @@
  * License: IPL
  */
 
-function add_last_nav_item($items) {
-    if ( is_user_logged_in() ) {
+function add_last_nav_item($items, $args) {
+    if (is_user_logged_in() && $args->theme_location == 'primary') {
         global $current_user;
         get_currentuserinfo();
 
         //$items .= '<li><a href="'.get_bloginfo( 'url' ).'/edit-profile" role="button" data-toggle="modal">Edit Profile</a></li>';
 
         $items .= '<li><a href="javascript:void(0)" role="button" data-toggle="modal">'.$current_user->display_name.'</a>';
-        $items .= '<ul>';
-        $items .= '<li><a href="'.get_site_url().'/my-account/edit-account" role="button" data-toggle="modal">Edit Profile</a></li>';
-        $items .= '<li><a href="'.get_site_url( 'url' ).'/my-account" role="button" data-toggle="modal">My Account</a></li>';
         $items .= '<li><a href="'. wp_logout_url(get_bloginfo( 'url' )).'" role="button" data-toggle="modal"> logout</a>';
-        $items.='</ul></li>';
+       
+       // Add Other Menus Add Css For Sub Menu
+        //$items .= '<ul>';
+        //$items .= '<li><a href="'.get_site_url().'/my-account/edit-account" role="button" data-toggle="modal">Edit Profile</a></li>';
+        //$items .= '<li><a href="'.get_site_url( 'url' ).'/my-account" role="button" data-toggle="modal">My Account</a></li>';
+        //$items.='</ul></li>';
         return $items;
-    } else {
+    }elseif (!is_user_logged_in() && $args->theme_location == 'primary') {
        $items .= '<li><a href="'.get_site_url( ).'/my-account/" role="button" data-toggle="modal">Login</a></li>';
        return $items .= '<li><a href="'.get_site_url( ).'/my-account/" role="button" data-toggle="modal">Register</a></li>';
     }
-
 }
+add_filter('wp_nav_menu_items','add_last_nav_item', 10, 2 );
+
 
 
 add_filter('wp_nav_menu_items','add_last_nav_item');
